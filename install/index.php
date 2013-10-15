02a327e6bea0bd76361dfe755312fa8757a4e4b3
Frank Houweling <houweling.frank@gmail.com>
2013-10-15 13:00:22 +0200

    Initial start on Document class

---
+++ install/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ *
+ * Build script to insert the data in [telegraaf-data] into the newly created elasticsearch server.
+ * Please make sure the /config.php configuration settings are correct. 
+ *
+ */
+
+
+?>
---
+++ searchengine/document.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * 
+ * Represents a document found with the query
+ *
+ */
+
+class Document{
+   
+   public function __construct( $docId, $docTitle, $docAuthor, $docUrl, $docContent ){
+   }
+   
+   // Getters
+   
+   public function getId(){
+      return $this->id;
+   }
+   
+   public function getTitle(){
+      return $this->title;
+   }
+   
+   public function getAuthor(){
+      return $this->author;  
+   }
+   
+   public function getUrl(){
+      return $this->url;  
+   }
+   
+   // Setters
+   
+   /**
+    * 
+    * Called after each setter to save data to ElasticSearch
+    *
+    */
+   private function save(){
+      // @todo Implement ElasticSearch
+   }
+   
+   /**
+    * 
+    * Construct a new Document instance from a JSON result-document.
+    *
+    */
+   static public function buildFromJson( $jsonText ){
+      $content = json_decode( $jsonText );
+      return new Document($content['id'], $content['title'], 
+                          $content['author'], $content['url'], $content['content']);
+   }
+   
+}
+
+?>
