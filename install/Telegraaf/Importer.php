2890d76f6716196ea365bbb89da10784c7bd27ef
Frank Houweling <houweling.frank@gmail.com>
2013-10-24 17:29:35 +0200

    Pie chart with persons

--- install/Telegraaf/Importer.php
+++ install/Telegraaf/Importer.php
@@ -9,7 +9,7 @@ class ImportDirNotWritableException extends Exception{}
 
 class TelegraafImporter implements Importer{
    
-   public function runImport($dataSrc = "Telegraaf/Data/1975/6/"){
+   public function runImport($dataSrc = "Telegraaf/Data/1975/7/"){
       $handle = opendir($dataSrc);
       if (!$handle) {
          throw new ImportDirNotWritableException();
