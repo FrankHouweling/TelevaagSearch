0f886c5dd55b44210e1af10c4b079fd8ccfdf4b5
Frank Houweling <houweling.frank@gmail.com>
2013-10-24 20:33:02 +0200

    Don't mind me.

--- install/Telegraaf/Importer.php
+++ install/Telegraaf/Importer.php
@@ -9,7 +9,7 @@ class ImportDirNotWritableException extends Exception{}
 
 class TelegraafImporter implements Importer{
 
-   public function runImport($dataSrc = "Telegraaf/Data/1975/9/"){
+   public function runImport($dataSrc = "Telegraaf/Data/1988/1/"){
       $handle = opendir($dataSrc);
       if (!$handle) {
          throw new ImportDirNotWritableException();
